c12b5e8bf341864a7866d14050ecf447a99ea0e0
gsuss <r.mahadevan@gsusstarsoftware.com>
2024-04-05 15:07:02 +0530

    Final Commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:quizapp/startscreen.dart';
+import 'package:flutter/src/painting/box_decoration.dart';
+import 'package:quizapp/quiz.dart';
+
+void main() {
+  runApp(
+    const Quiz(),
+  );
+}
